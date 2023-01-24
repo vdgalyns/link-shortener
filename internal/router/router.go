f3f56bbbec4dbc6e87e27c82e598f87b42c977f7
Vladimir Galynsky <vdgalyns@mts.ru>
2023-01-24 23:30:55 +0300

    Add gzip compress middleware

--- internal/router/router.go
+++ internal/router/router.go
@@ -4,6 +4,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/vdgalyns/link-shortener/internal/handler"
+	middle "github.com/vdgalyns/link-shortener/internal/router/middleware"
 )
 
 func NewRouter(h *handler.Handler) chi.Router {
@@ -13,6 +14,7 @@ func NewRouter(h *handler.Handler) chi.Router {
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+	r.Use(middle.GzipHandle)
 
 	r.Get("/{hash}", h.Get)
 	r.Post("/", h.Add)
