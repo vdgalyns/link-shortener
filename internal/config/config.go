4f1e7409c0adda4c024ac7042084aacf3b734f0d
Vladimir Galynsky <vdgalyns@mts.ru>
2023-01-24 16:26:05 +0300

    Add flags

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,12 +1,19 @@
 package config
 
 import (
+	"flag"
 	"github.com/caarlos0/env/v6"
 )
 
+const (
+	ServerAddress   string = ":8080"
+	BaseURL         string = "http://localhost:8080"
+	FileStoragePath string = "data.txt"
+)
+
 type Config struct {
-	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:":8080"`
-	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
+	ServerAddress   string `env:"SERVER_ADDRESS"`
+	BaseURL         string `env:"BASE_URL"`
 	FileStoragePath string `env:"FILE_STORAGE_PATH"`
 }
 
@@ -14,6 +21,31 @@ func NewConfig() (*Config, error) {
 	var cfg Config
 	if err := env.Parse(&cfg); err != nil {
 		return nil, err
+	}
+	a := flag.String("a", "", "server address")
+	b := flag.String("b", "", "base url")
+	f := flag.String("f", "", "file storage path")
+	flag.Parse()
+	if cfg.ServerAddress == "" {
+		if *a == "" {
+			cfg.ServerAddress = ServerAddress
+		} else {
+			cfg.ServerAddress = *a
+		}
+	}
+	if cfg.BaseURL == "" {
+		if *b == "" {
+			cfg.BaseURL = BaseURL
+		} else {
+			cfg.BaseURL = *b
+		}
+	}
+	if cfg.FileStoragePath == "" {
+		if *f == "" {
+			cfg.FileStoragePath = FileStoragePath
+		} else {
+			cfg.FileStoragePath = *f
+		}
 	}
 	return &cfg, nil
 }
