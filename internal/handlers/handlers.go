3fb77796663712127dbac236b7b7d25ae207ccbf
Vladimir Galynsky <vdgalyns@mts.ru>
2023-02-05 22:49:04 +0300

    Change response status code on batch add route

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -168,6 +168,7 @@ func (h *Handlers) AddBatch(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
 	w.Write(outputJSON)
 }
 
